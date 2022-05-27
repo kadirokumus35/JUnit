55d42052264b797133f1804f8d1de876a78436cb
kadir <kdrokumus35@gmail.com>
2022-05-27 23:38:41 +0300

    rename

--- src/test/java/JUnitGiris/C01_JUnit_Ilk_Class.java
+++ src/test/java/JUnitGiris/C01_JUnit_Ilk_Class.java
@@ -1,4 +1,4 @@
-package mavenGiris;
+package JUnitGiris;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class C01_Maven_Ilk_Class {
+public class C01_JUnit_Ilk_Class {
     public static void main(String[] args) {
 
         WebDriverManager.chromedriver().setup();
