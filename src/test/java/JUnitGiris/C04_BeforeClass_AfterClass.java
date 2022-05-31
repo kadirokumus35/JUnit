2b0f9f07d6969787bd0c2485baf01284bf87a190
kadir <kdrokumus35@gmail.com>
2022-05-31 23:48:30 +0300

    class

---
+++ src/test/java/JUnitGiris/C04_BeforeClass_AfterClass.java
@@ -0,0 +1,47 @@
+package JUnitGiris;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C04_BeforeClass_AfterClass {
+    /*
+   @BeforeClass ve @AfterClass notasyonlari sadece static methodlar icin calisir
+    */
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+    @AfterClass
+    public static void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void test01(){
+        // test notasyonu before call yapar
+        driver.get("https://www.amazon.com");
+        // test notasyonu after call yapar
+    }
+
+    @Test
+    public void test02(){
+        driver.get("https://www.techproeducation.com");
+    }
+
+    @Test
+    public void test03(){
+        driver.get("https://www.facebook.com");
+    }
+
+}
