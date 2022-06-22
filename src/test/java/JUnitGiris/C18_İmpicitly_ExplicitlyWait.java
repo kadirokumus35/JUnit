4cb0e9c9c6909257bd8f0f03bdda909ad5f52b68
kadir <kdrokumus35@gmail.com>
2022-06-22 23:12:00 +0300

    wait

--- src/test/java/JUnitGiris/C18_İmpicitly_ExplicitlyWait.java
+++ src/test/java/JUnitGiris/C18_İmpicitly_ExplicitlyWait.java
@@ -4,9 +4,13 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.TestBase;
 
-public class C18_ExplicitlyWait extends TestBase {
+import java.time.Duration;
+
+public class C18_İmpicitly_ExplicitlyWait extends TestBase {
 
     // Iki tane metod olusturun : implicitWait() , explicitWait()
     // Iki metod icin de asagidaki adimlari test edin.
@@ -34,6 +38,20 @@ public class C18_ExplicitlyWait extends TestBase {
 
     @Test
     public void explicitlyWait (){
+        //3. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
 
+        //4. Remove butonuna basin.
+        driver.findElement(By.xpath("//button[@onclick='swapCheckbox()']")).click();
+
+        //5. “It’s gone!” mesajinin goruntulendigini dogrulayin.,
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        WebElement itsGone= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[@id='message']")));
+
+        //6. Add buttonuna basin
+        driver.findElement(By.xpath("(//button[@autocomplete='off'])[1]")).click();
+
+        //7. It’s back mesajinin gorundugunu test edin
+        System.out.println(driver.findElement(By.xpath("//p[@id='message']")).isDisplayed());
     }
 }
